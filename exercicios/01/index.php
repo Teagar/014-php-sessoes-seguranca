edf561ea4a73bd6f98e8176fc309acbf5d18879e
Thiago Cerqueira <81341250+Teagar@users.noreply.github.com>
2026-09-15 17:22:12 -0300

    Update index.php

--- exercicios/01/index.php
+++ exercicios/01/index.php
@@ -1,5 +1,5 @@
 <?php
 declare(strict_types=1);
 
-// TODO: crie o cookie tema=escuro por 7 dias, com path / e SameSite=Lax.
+// TODO: crie o cookie tema=escuro por 7 dias
 // Explique em comentário por que headers devem ser enviados antes do HTML.
